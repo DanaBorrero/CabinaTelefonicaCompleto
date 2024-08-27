1b844be724011a6cf2c370eb6f8d8cc17a4e562e
DanaBorrero <danaborrero@hotmail.com>
2024-08-27 11:04:25 -0500

    CabinaTel_Final

--- src/CabinaTel.kt
+++ src/CabinaTel.kt
@@ -3,7 +3,8 @@ import kotlin.random.Random
 class CabinaTel(var nomCab: String) {
     var crearCab : MutableList<CabinaTel> = mutableListOf()
     val minTotal = Random.nextInt(1,101)
-
+    var numero : Int = 0
+    var cosTotal : Int = 0
 
     fun crearCabina(): CabinaTel {
         println("Ingrese el nombre de la nueva cabina: ")
@@ -15,13 +16,13 @@ class CabinaTel(var nomCab: String) {
     }
     fun numLlam() {
         println("Ingrese el número de teléfono que desea: ")
-        var numero : Int = readln().toInt()
+        numero = readln().toInt()
         println("Llamando al número $numero desde la cabina $nomCab.")
     }
 
     fun cosTotal(valor: Int){
         println("Minutos asignado a esta llamada: $minTotal")
-        var cosTotal = minTotal*valor
+        cosTotal = minTotal*valor
         println("El costo total de la llamada es de: $cosTotal")
     }
 
@@ -32,12 +33,12 @@ class CabinaTel(var nomCab: String) {
         } else {
             println("Llamadas registradas:")
             crearCab.forEachIndexed { index, llamada ->
-                println("${index + 1}. Número: ${llamada.numLlam()}, Duración: ${llamada.minTotal} minutos, Costo: ${llamada.cosTotal(0)} pesos")
+                println("${index + 1}. Número: ${llamada.numero}, Duración: ${llamada.minTotal} minutos, Costo: ${llamada.cosTotal(0)} pesos")
             }
         }
     }
     override fun toString(): String {
-        return "CabinaTel(numCab=$nomCab)"
+        return "CabinaTel(nomCab='$nomCab', crearCab=$crearCab, minTotal=$minTotal, numero=$numero, cosTotal=$cosTotal)"
     }
 
 
